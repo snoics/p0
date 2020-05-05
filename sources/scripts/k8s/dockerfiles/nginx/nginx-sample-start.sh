e998a068fcc28185c902343b36c281a736ff2282
snoics <13262455@qq.com>
2020-05-06 00:40:55 +0800

    change script

--- sources/scripts/k8s/dockerfiles/nginx/nginx-sample-start.sh
+++ sources/scripts/k8s/dockerfiles/nginx/nginx-sample-start.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 APPNAME=p0
-VOLUME_PATH=/home/p0/disk/${APPNAME}
+VOLUME_PATH=/home/p0/disk/${APPNAME}/nginx
 
 echo "nginx ${APPNAME} server deploy and start ......"
 
-if [ ! -d "${VOLUME_PATH}/config/nginx" ];
+if [ ! -d "${VOLUME_PATH}" ];
 then
-  mkdir -p ${VOLUME_PATH}/config/nginx
-  mkdir -p ${VOLUME_PATH}/applications/nginx
-  mkdir -p ${VOLUME_PATH}/data/nginx
-  mkdir -p ${VOLUME_PATH}/logs/nginx
-  cp -rf conf ${VOLUME_PATH}/config/nginx
-  chown -R systemd-coredump:root ${VOLUME_PATH}/config/nginx
-  chown -R systemd-coredump:root ${VOLUME_PATH}/applications/nginx
-  chown -R systemd-coredump:root ${VOLUME_PATH}/data/nginx
-  chown -R systemd-coredump:root ${VOLUME_PATH}/logs/nginx
+  mkdir -p ${VOLUME_PATH}/config
+  mkdir -p ${VOLUME_PATH}/applications
+  mkdir -p ${VOLUME_PATH}/data
+  mkdir -p ${VOLUME_PATH}/logs
+  cp -rf conf ${VOLUME_PATH}/config
+  chown -R systemd-coredump:root ${VOLUME_PATH}
 fi
 
 docker run --name nginx-${APPNAME}-1.0 \
     -p 90:80 \
-    -v ${VOLUME_PATH}/config/nginx/conf:/opt/config/nginx \
-    -v ${VOLUME_PATH}/applications/nginx:/opt/applications/nginx \
-    -v ${VOLUME_PATH}/data/nginx:/opt/data/nginx \
-    -v ${VOLUME_PATH}/logs/nginx:/opt/logs/nginx \
+    -v ${VOLUME_PATH}/config/conf:/opt/p0/nginx/config \
+    -v ${VOLUME_PATH}/applications:/opt/p0/nginx/applications \
+    -v ${VOLUME_PATH}/data:/opt/p0/nginx/data \
+    -v ${VOLUME_PATH}/logs:/opt/p0/nginx/logs \
     -d --rm nginx:p0-1.0
 
 ##################################################################
-#  http://localhost/home/       /opt/applications/nginx/home
-#  http://localhost/console/    /opt/applications/nginx/console
+#  PC端网站
+#  http://localhost/home/       /opt/p0/nginx/applications/home
+#
+#  后台管理
+#  http://localhost/console/    /opt/p0/nginx/applications/console
+#
+#  移动端
+#  http://localhost/m/    /opt/p0/nginx/applications/m
+#
+#  后端API
 #  http://localhost/api/        localhost:8080/api/
 #
 ##################################################################
